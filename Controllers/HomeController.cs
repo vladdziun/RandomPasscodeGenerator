0596084d5e195b6af0681c009f93eed4c699c84a
unknown <v.dziun@gmail.com>
2019-04-07 19:00:52 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RandomPasscode.Models;
+
+namespace RandomPasscode.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            TempData["RandomString"] = RandomString(14);
+            ViewBag.String = TempData["RandomString"];
+            return View();
+        }
+
+    }
+}
